8eaca1157b7ec0f4f9b26b1e10c9c30c35a127e4
Yrjö Kari-Koskinen <yrjo.kari-koskinen@futurice.com>
2016-11-20 22:35:40 +0100

    lein new app pixelrator

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to pixelrator
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/pixelrator/core_test.clj
@@ -0,0 +1,7 @@
+(ns pixelrator.core-test
+  (:require [clojure.test :refer :all]
+            [pixelrator.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
